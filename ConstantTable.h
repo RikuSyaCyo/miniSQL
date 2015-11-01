4a63dc81ebafe3e7e0bef0983e1cd59a0655f344
tlsshh <598991258@qq.com>
2015-11-01 20:04:04 +0800

    constant table

---
+++ ConstantTable.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <string>
+#include "FilePosition.h"
+#include <vector>
+using namespace std;
+
+typedef vector<FilePosition> TupleResults;
+
+static const int ATTRIMAXNUM = 32; // max number of attributes
+static const bool NEWTABLE = true; // flag to new a table
+static const bool NEWTUPLE = true; // flag to new a tuple
+static const string TupleFilePostfix = "DATA"; // tuple file postfix
+static const int TABLENAMELENGTH = 128; //max length of table name
+
+//operation flag
+static const int EQUAL = 0;
+static const int NOEQUAL = 1;
+static const int LESSTHAN = 2;
+static const int GREATERTHAN = 3;
+static const int NOGREATERTHAN = 4;
+static const int NOLESSTHAN = 5;
+
+// type flag
+static const  int INT = 0;
+static const  int CHAR = 1;
+static const  int FLOAT = 2;
